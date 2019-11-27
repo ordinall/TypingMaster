092d08cb3761a40411c64a17d810495c1784dd50
Ordinall <abdullah4abidansari@gmail.com>
2019-11-27 22:34:23 +0500

    random word fetch function, commented unused code

--- main.cpp
+++ main.cpp
@@ -1,38 +1,94 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <string.h>
 #pragma comment(lib,"lib/pdcurses.lib")
 #include <windows.h>
+#include <time.h>
 #undef MOUSE_MOVED
 #include "include/curses.h"
 
-void destroy_win(WINDOW *local_win){
-	wborder(local_win, ' ', ' ', ' ',' ',' ',' ',' ',' ');
-    wclear(local_win);
-	wrefresh(local_win);
-	delwin(local_win);
+char * getword(FILE * fp){
+    fseek(fp, 0, SEEK_SET);
+    char * ch = (char *)malloc(100);
+    int i=0;
+    int temp=1;
+    while(temp != EOF){
+       // if(ch == '\n'){
+            temp = fscanf(fp,"%s",ch);
+            i++;
+      //  }
+      //  ch=fgetc(fp);
+    }
+    
+    fseek(fp, 0, SEEK_SET);
+    int ran = rand() % (i);
+    for(int j=1;j<=ran;j++){
+        fscanf(fp,"%s",ch);
+    }
+   /* ch ='a';
+    int pos=ftell(fp);
+    printf("%d",ftell(fp));
+    int len=0;
+    for(;ch != '\n';len++){
+        ch = fgetc(fp);
+    }
+    fseek(fp,pos,SEEK_SET); */
+   // char word[100];
+    //fgets(word,len-1,fp);
+    //for(int i=0;i<len;i++){
+    //    *(word+i)=fgetc(fp);
+    //}
+    //*(word+len)='\0';
+
+   // return word;
+   return ch;
 }
-WINDOW *create_newwin(int height, int width, int starty, int startx){	WINDOW *local_win;
-	local_win = newwin(height, width, starty, startx);
-	box(local_win, 0 , 0);
-    mvwprintw(local_win,1,1,"HENLO :)");
-	wrefresh(local_win);
-	return local_win;
+
+void destroy_win(WINDOW *wind){
+	wborder(wind, ' ', ' ', ' ',' ',' ',' ',' ',' ');
+    wclear(wind);
+	wrefresh(wind);
+	delwin(wind);
+}
+
+WINDOW *create_newwin(int h, int w, int y, int x,int i){	
+    WINDOW *wind;
+	wind = newwin(h, w, y, x);
+    if(i==1){
+	    box(wind, 0 , 0);
+    }
+	wrefresh(wind);
+	return wind;
 }
 
 
 int main(){
-    char ch;
+    srand(time(NULL));
+    while(1){
+        FILE * fp = fopen("diffi.txt","r");
+        Sleep(100);
+        char * haha=getword(fp);
+        printf("%s\n",haha);
+        free(haha);
+        fclose(fp);
+    }
+    getchar();
+
+}
+  /*  char ch;
     int wy,wx;
     initscr();
     noecho();
     cbreak();
-    start_color();
-    init_pair(1,COLOR_BLACK,COLOR_RED);
-    attron(COLOR_PAIR(1));
+   // start_color();
+   // init_pair(1,COLOR_BLACK,COLOR_RED);
+   // attron(COLOR_PAIR(1));
     for(int i=0;i < COLS;i++){
         for(int k=0;k<LINES;k++){
             mvprintw(k,i," ");
         }
-    }
+    } 
     curs_set(0);
     WINDOW *hello = newwin(3,10,LINES/2,COLS/2);
     //wattron(hello,COLOR_PAIR(1));
@@ -72,12 +128,12 @@ int main(){
             destroy_win(hello);
             hello = create_newwin(3,10,wy+1,wx);
         }
-     /*   attron(COLOR_PAIR(1));
+        attron(COLOR_PAIR(1));
         for(int i=0;i < COLS;i++){
             for(int k=0;k<LINES;k++){
                 mvprintw(k,i," ");
             }
-        }*/
+        }
         refresh();
         wrefresh(hello);        
         
@@ -85,3 +141,4 @@ int main(){
     getch();
     endwin();
 }
+*/

9bd1c0369b99d861430b6ba0e99ced496403687a
Abdullah Ansari <abdullah4abidansari@gmail.com>
2019-12-02 00:01:47 +0500

    Profile system implemented

--- main.cpp
+++ main.cpp
@@ -8,42 +8,45 @@
 #undef MOUSE_MOVED
 #include "include/curses.h"
 
+
+
+
+int wordcountfile=-1;
+void profiles(FILE * profile);
+
+
+
+
+struct player{
+    char name[40];
+    int wpm=0;
+    int score=0;
+} play;
+
+
+
+
 char * getword(FILE * fp){
     fseek(fp, 0, SEEK_SET);
     char * ch = (char *)malloc(100);
-    int i=0;
     int temp=1;
-    while(temp != EOF){
-       // if(ch == '\n'){
-            temp = fscanf(fp,"%s",ch);
-            i++;
-      //  }
-      //  ch=fgetc(fp);
+    if(wordcountfile==-1){
+        while(temp != EOF){
+                temp = fscanf(fp,"%s",ch);
+                wordcountfile++;
+        }
     }
-    
     fseek(fp, 0, SEEK_SET);
-    int ran = rand() % (i);
+    int ran = rand() % (wordcountfile);
     for(int j=1;j<=ran;j++){
         fscanf(fp,"%s",ch);
     }
-   /* ch ='a';
-    int pos=ftell(fp);
-    printf("%d",ftell(fp));
-    int len=0;
-    for(;ch != '\n';len++){
-        ch = fgetc(fp);
-    }
-    fseek(fp,pos,SEEK_SET); */
-   // char word[100];
-    //fgets(word,len-1,fp);
-    //for(int i=0;i<len;i++){
-    //    *(word+i)=fgetc(fp);
-    //}
-    //*(word+len)='\0';
-
-   // return word;
    return ch;
 }
+
+
+
+
 
 void destroy_win(WINDOW *wind){
 	wborder(wind, ' ', ' ', ' ',' ',' ',' ',' ',' ');
@@ -52,93 +55,164 @@ void destroy_win(WINDOW *wind){
 	delwin(wind);
 }
 
-WINDOW *create_newwin(int h, int w, int y, int x,int i){	
-    WINDOW *wind;
-	wind = newwin(h, w, y, x);
-    if(i==1){
-	    box(wind, 0 , 0);
-    }
-	wrefresh(wind);
-	return wind;
+
+
+
+
+void stringhcentre(int y,int x, char * tempword){
+    mvprintw(y,x - strlen(tempword)/2 ,"%s",tempword);
 }
+
+
+
+
+
+void delprofile(char *filename, int recnum) {
+	FILE *fp = fopen(filename, "r+");
+	FILE *ftemp=fopen("tempo", "w+");
+	struct player temp;
+	for(int i=0;fread(&temp,sizeof(struct player),1,fp) != NULL;i++) {
+		if(i == recnum){
+        }
+	    else {
+			fwrite(&temp, sizeof(struct player), 1, ftemp);
+		}
+	}
+	fclose(fp);
+	fclose(ftemp);
+	remove(filename);
+	rename("tempo", filename);
+    FILE * profile = fopen("profile.dat","r+");
+    profiles(profile);
+}
+
+
+
+
+void createprofile(FILE * profile){
+    struct player temp;
+    box(stdscr,0,0);
+    fseek(profile,0,SEEK_END);
+    stringhcentre(LINES/2,COLS/2-10,"Enter name:");
+    echo();
+    refresh();
+    char a;
+    int i=0;
+    for(;!(( ( a=getch() ) == '\n') || i >= 38); i++){
+            temp.name[i] = a;
+    }
+    temp.name[i] = '\0';
+    noecho();
+    fwrite(&temp,sizeof(struct player),1,profile);
+    clear();
+    refresh();
+    profiles(profile);
+}
+
+
+
+
+
+void profiles(FILE * profile){
+    stringhcentre(2,COLS/2,"PROFILES");
+    stringhcentre(3,COLS/2,"press DEL key to delete a profile");
+    WINDOW *prowin = newwin(LINES-10,COLS/2 - 10,5,COLS/2-24);
+    box(prowin,0,0);
+    box(stdscr,0,0);
+    refresh();
+    wrefresh(prowin);
+    fseek(profile,0,SEEK_SET);
+    int totallist=0;
+    for(;;totallist++){
+        fread(&play, sizeof(struct player), 1, profile);
+        mvwprintw(prowin,totallist+2,3,"%s",play.name);
+        if(feof(profile)){
+            break;
+        }
+    }
+    mvwprintw(prowin,totallist+2,3,"\r                                             ");
+    mvwprintw(prowin,totallist+2,3,"MAKE A NEW PROFILE");
+    box(prowin,0,0);
+    wrefresh(prowin);
+    refresh();
+    if(totallist>14){
+        wprintw(prowin,"\rPROFILE LIMIT EXCEEDED, DELETE A PROFILE");
+        wrefresh(prowin);
+    }
+    int select = 0;
+    keypad(stdscr,TRUE);
+    while(1){
+        mvwprintw(prowin,select+2,2,">");
+        wrefresh(prowin);
+        switch(getch()){
+            case KEY_UP:
+                mvwprintw(prowin,select+2,2," ");
+                select--;
+                if(select == -1){
+                    select = totallist;
+                }
+                break;
+            case KEY_DOWN:
+                mvwprintw(prowin,select+2,2," ");
+                select++;
+                if(select>totallist){
+                    select = 0;
+                }
+                break;
+            case '\n':
+                if(select == totallist){
+                    delwin(prowin);
+                    keypad(stdscr,FALSE);
+                    createprofile(profile);
+                }
+                break;
+            case KEY_DC:
+                delwin(prowin);
+                keypad(stdscr,FALSE);
+                fclose(profile);
+                delprofile("profile.dat",select);
+                break;    
+        }
+    }
+    
+    refresh();
+    wrefresh(prowin);
+}
+
+
 
 
 int main(){
     srand(time(NULL));
-    while(1){
-        FILE * fp = fopen("glo.txt","r");
-        Sleep(100);
-        char * haha=getword(fp);
-        printf("%s\n",haha);
-        free(haha);
-        fclose(fp);
-    }
-    getchar();
-
-}
-  /*  char ch;
-    int wy,wx;
+    FILE * wordp = fopen("glo.txt","r");
     initscr();
     noecho();
-    cbreak();
-   // start_color();
-   // init_pair(1,COLOR_BLACK,COLOR_RED);
-   // attron(COLOR_PAIR(1));
-    for(int i=0;i < COLS;i++){
-        for(int k=0;k<LINES;k++){
-            mvprintw(k,i," ");
-        }
-    } 
     curs_set(0);
-    WINDOW *hello = newwin(3,10,LINES/2,COLS/2);
-    //wattron(hello,COLOR_PAIR(1));
-    box(hello,0,0);
+    int wy,wx;
     box(stdscr,0,0);
-    mvwprintw(hello,'1','1'," ");
+    stringhcentre(LINES/2 - 3, COLS/2,"Welcome to Typing Master 0.0.1 Alpha");
+    stringhcentre(LINES/2 - 2, COLS/2,"Developed by Abdullah Abid (19K-1042) SE-A");
+    stringhcentre(LINES/2 - 1, COLS/2,"Press Enter to Continue");
     refresh();
-    wrefresh(hello);
-    while(1){
+    char a = 'a';
+    while(a != '\n'){
+        a=getch();
+    }
+    FILE * profile = fopen("profile.dat","r+");
+    if(profile == NULL){
+        profile = fopen("profile.dat","w+");
+    }
+    clear();
+    box(stdscr,0,0);
+    profiles(profile);
+    endwin();
+
+}
+  /*
         ch = getch();
         getbegyx(hello,wy,wx);
         if(wy == 1){
             wy++;
         }
-        if(wy == LINES-4){
-            wy--;
-        } 
-        if(wx == 1){
-            wx++;
-        }
-        if(wx == COLS - 11){
-            wx--;
-        }
-        if(ch == 'w'){
-            destroy_win(hello);
-            hello = create_newwin(3,10,wy-1,wx);
-        }
-        if(ch == 'd'){
-            destroy_win(hello);
-            hello = create_newwin(3,10,wy,wx+1);
-        }
-        if(ch == 'a'){
-            destroy_win(hello);
-            hello = create_newwin(3,10,wy,wx-1);
-        }
-        if(ch == 's'){
-            destroy_win(hello);
-            hello = create_newwin(3,10,wy+1,wx);
-        }
-        attron(COLOR_PAIR(1));
-        for(int i=0;i < COLS;i++){
-            for(int k=0;k<LINES;k++){
-                mvprintw(k,i," ");
-            }
-        }
-        refresh();
-        wrefresh(hello);        
         
-    }
-    getch();
-    endwin();
-}
 */
